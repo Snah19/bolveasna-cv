3550390d1c6547e33a235a6f9e3356ce95a312fc
bolveasna <vibolveasna2004@gmail.com>
2025-10-06 19:57:34 +0700

    Initialized the project

---
+++ app/page.jsx
@@ -0,0 +1,78 @@
+import { roboto } from "@/fonts";
+
+const HomePage = () => {
+  return (
+    <main className="w-[1000px] mx-auto p-10">
+      <h1 className="text-2xl font-bold text-center">BOL VEASNA</h1>
+      <p className={`${roboto.className} text-sm text-center text-gray-700`}>Front-end Developer</p>
+      <p className={`${roboto.className} text-sm text-center text-gray-700`}>+855 96 273 277 6 &nbsp; &bull; &nbsp; vibolveasna2004@gmail.com &nbsp; &bull; &nbsp; linkedin.com/in/veasna-bol-14538328b &nbsp; &bull; &nbsp; github.com/Snah19 </p>
+      <div className="my-5 space-y-5">
+        <div>
+          <h2 className="text-xl text-center">Summary</h2>
+          <hr />
+          <p className={`${roboto.className} text-sm text-gray-700`}>After hundreds of hours of learning, practicing, and building various projects including small demos, Frontend Mentor challenges, and 2 personal full-stack web applications. I am confident in my ability to bring Figma designs to life and am ready to pursue a professional career in Front-end Development.</p>
+        </div>
+
+        <div>
+          <h2 className="text-xl text-center">Education</h2>
+          <hr />
+          <div className="flex justify-between">
+            <h3>SETEC Institute</h3>
+            <h3 className={`${roboto.className}`}>Phnom Penh, Cambodia</h3>
+          </div>
+          <div className="flex justify-between text-gray-700">
+            <p className={`${roboto.className} text-sm`}>Management Information System | GPA: <span className="font-bold text-black">3.89</span>/4.0</p>
+            <p className={`${roboto.className} text-sm`}>2023 - 2025</p>
+          </div>
+          <p className={`${roboto.className} text-sm text-gray-700`}>Relevant courses: HTML, CSS, C++, C#, Data Structures & Algorithms, Data Modeling, MySQL, Microsoft SQL Server</p>
+        </div>
+
+        <div>
+          <h2 className="text-xl text-center">Industry Skills</h2>
+          <hr />
+          <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Front-end : </span> HTML &nbsp; &bull; &nbsp; CSS &nbsp; &bull; &nbsp; JavaScript, &nbsp; &bull; &nbsp; TypeScript &nbsp; &bull; &nbsp; Tailwind CSS &nbsp; &bull; &nbsp; <span className="font-bold text-black">React.js</span> &nbsp; &bull; &nbsp; <span className="font-bold text-black">Next.js</span></p>
+          <p className={`${roboto.className} text-sm text-gray-700`}><span className="text-black">Back-end : </span> Node.js &nbsp; &bull; &nbsp; Express.js &nbsp; &bull; &nbsp; MongoDB &nbsp; &bull; &nbsp; SQL &nbsp; &bull; &nbsp; RESTFul APIs</p>
+          <p className={`${roboto.className} text-sm text-gray-700`}><span className="text-black">Libraries & Tools : </span>Cloudinary &nbsp; &bull; &nbsp; React Query &nbsp; &bull; &nbsp; NextAuth &nbsp; &bull; &nbsp; Socket.io &nbsp; &bull; &nbsp; Postman &nbsp; &bull; &nbsp; Figma</p>
+          <p className={`${roboto.className} text-sm text-gray-700`}><span className="text-black">Source Control : </span> Git &nbsp; &bull; &nbsp; GitHub </p>
+        </div>
+
+        <div>
+          <h2 className="text-xl text-center">Certifications</h2>
+          <hr />
+          <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">SETEC Institute : </span>Certificate of foundation year course</p>
+          <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Udemy : </span> Next.js From Scratch by Brad Traversy</p>
+        </div>
+
+        <div>
+          <h2 className="text-xl text-center">Strengths</h2>
+          <hr />
+          <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Eager to learn new skills : </span> As a person who is starting out, I am constantly seeking for new ways to grow and expand my skill set.</p>
+          <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Not afraid of failure : </span> Strong believer in the mantra that we learn from our mistakes.</p>
+          <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Time Management : </span> The habit of planning before executing and setting reasonable deadlines saved me at least 25% of my productive time.</p>
+          <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Collaborative : </span> With my current knowledge and abilities, I can confidently communicate with Front-end developers, Backend developers, Designers, and even Clients.</p>
+        </div>
+
+        <div>
+          <h2 className="text-xl text-center">Personal Projects</h2>
+          <hr />
+          <h3 className="text-lg">Property KH - A Property Listing Site</h3>
+          <div className="space-y-2">
+            <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Features : </span> Authentication (Google Accounts) &nbsp; &bull; &nbsp; Browse Properties &nbsp; &bull; &nbsp; Search for specific properties &nbsp; &bull; &nbsp;  Message property owners &nbsp; &bull; &nbsp; Post, Update, Delete, Bookmark Properties &nbsp; &bull; &nbsp; View User's Profile</p>
+            <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Technologies : </span> React.js &nbsp; &bull; &nbsp; Next.js &nbsp; &bull; &nbsp; Tailwind CSS &nbsp; &bull; &nbsp; Express.js &nbsp; &bull; &nbsp; MongoDB &nbsp; &bull; &nbsp; NextAuth &nbsp; &bull; &nbsp; Cloudinary &nbsp; &bull; &nbsp; Other minor libraries...</p>
+            <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Source code : </span> (Front-end) <a className="text-blue-500" href="https://github.com/Snah19/property-kh">https://github.com/Snah19/property-kh</a> &nbsp; &bull; &nbsp; (Back-end) <a className="text-blue-500" href="https://github.com/Snah19/property-kh-api">https://github.com/Snah19/property-kh-api</a></p>
+            <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Live website : </span> <a className="text-blue-500" href="https://property-kh-75gh.vercel.app">https://property-kh-75gh.vercel.app</a></p>
+          </div>
+          <h3 className="mt-5 text-lg">Property KH - A Property Listing Site</h3>
+          <div className="space-y-2">
+            <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Features : </span> </p>
+            <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Technologies : </span> </p>
+            <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Source code : </span> (Front-end) <a className="text-blue-500" href="https://github.com/Snah19/x-frontend">https://github.com/Snah19/x-frontend</a> &nbsp; &bull; &nbsp; (Back-end) <a className="text-blue-500" href="https://github.com/Snah19/x-backend">https://github.com/Snah19/x-backend</a></p>
+            <p className={`${roboto.className} text-sm text-gray-700`}> <span className="text-black">Live website : </span> <a className="text-blue-500" href="https://x-frontend-three.vercel.app">https://x-frontend-three.vercel.app</a></p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
---
+++ fonts.js
@@ -0,0 +1,11 @@
+import { Volkhov, Roboto } from "next/font/google";
+
+export const volkhov = Volkhov({
+  subsets: ["latin"],
+  weight: ["400", "700"]
+});
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"]
+});
